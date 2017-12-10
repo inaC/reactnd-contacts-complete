423b034ec9809e114f2f8d0a29fc423ec298efa1
Inacio <inaciogcerqueira@gmail.com>
2017-12-10 16:59:29 -0200

    Adds showing-contact element to the ListContacts render method

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
-/*
-	we are going to build a component which will display the contact list :)
-	such component will receive the contact list 
-*/
-
 
 class ListContacts extends Component {
 	static propTypes = {
@@ -20,14 +15,19 @@ class ListContacts extends Component {
 	}
 
 	setQuery = (query) => this.setState({query: query.trim()})
+
+	clearQuery = () => this.setState({query: ''})
 	
-	searchContacts = (match) => (this.state.query ? (this.props.contacts.filter((contact) => match.test(contact.name))) : this.props.contacts)
+	searchContacts = (match, contacts) => (this.state.query ? (contacts.filter((contact) => match.test(contact.name))) : contacts)
 	
-	showingContacts = (orderBy, match) => (this.searchContacts(match).sort(sortBy(orderBy)))
+	showingContacts = (match, contacts, orderBy) => (this.searchContacts(match, contacts).sort(sortBy(orderBy)))
 	
 	render() {
-		let match = new RegExp(escapeRegExp(this.state.query), 'i')
-		let contacts = this.showingContacts('name', match)
+		const {contacts, onDeleteContact} = this.props
+		const {query} = this.state
+		const match = new RegExp(escapeRegExp(query), 'i')
+		const showingContacts = this.showingContacts(match, contacts, 'name')
+
 		return(
 			<div className='list-contacts'>
 				<div className='list-contacts-top'>
@@ -35,21 +35,25 @@ class ListContacts extends Component {
 						className = 'search-contacts'
 						type='text' 
 						placeholder='Search for contact'
-						value={this.state.query}
-						onChange={(event) => this.setQuery(event.target.value)}/>
+						value={query}
+						onChange={(event) => this.setQuery(event.target.value)} 
+					/>
 				</div>
+				{contacts.length !== showingContacts.length && (
+					<div className = 'showing-contacts'>
+						<span>	Now showing {showingContacts.length} of {contacts.length} total </span>
+						<button onClick = {this.clearQuery }> Show all </button>
+				  </div>)
+				}
 				<ol className='contact-list'>
-					{contacts.map((contact, index) => 
+					{showingContacts.map((contact, index) => 
 						<li key={contact.id} className='contact-list-item'> 
-							<div className='contact-avatar' style = {{ backgroundImage: `url(${contact.avatarURL})`}}> 
-							</div>
+							<div className='contact-avatar' style = {{ backgroundImage: `url(${contact.avatarURL})`}}> </div>
 							<div className='contact-details'>
 								<p> {contact.name} </p>
 								<p> {contact.email} </p>
 							</div>
-							<button onClick = {()=>this.props.onDeleteContact(contact)} className='contact-remove' > 
-								Remove 
-							</button>
+							<button onClick = {()=> onDeleteContact(contact)} className='contact-remove'> Remove </button>
 						</li>)}
 				</ol>
 			</div>
