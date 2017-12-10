30ed8fdd60eabb6c75a9947254f23345cd34aa28
Inacio <inaciogcerqueira@gmail.com>
2017-12-10 14:01:32 -0200

    Adds contact filtering based on input value of the form

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 /*
 	we are going to build a component which will display the contact list :)
 	such component will receive the contact list 
@@ -18,8 +20,14 @@ class ListContacts extends Component {
 	}
 
 	setQuery = (query) => this.setState({query: query.trim()})
-
+	
+	searchContacts = (match) => (this.state.query ? (this.props.contacts.filter((contact) => match.test(contact.name))) : this.props.contacts)
+	
+	showingContacts = (orderBy, match) => (this.searchContacts(match).sort(sortBy(orderBy)))
+	
 	render() {
+		let match = new RegExp(escapeRegExp(this.state.query), 'i')
+		let contacts = this.showingContacts('name', match)
 		return(
 			<div className='list-contacts'>
 				<div className='list-contacts-top'>
@@ -31,7 +39,7 @@ class ListContacts extends Component {
 						onChange={(event) => this.setQuery(event.target.value)}/>
 				</div>
 				<ol className='contact-list'>
-					{this.props.contacts.map((contact, index) => 
+					{contacts.map((contact, index) => 
 						<li key={contact.id} className='contact-list-item'> 
 							<div className='contact-avatar' style = {{ backgroundImage: `url(${contact.avatarURL})`}}> 
 							</div>
@@ -42,7 +50,6 @@ class ListContacts extends Component {
 							<button onClick = {()=>this.props.onDeleteContact(contact)} className='contact-remove' > 
 								Remove 
 							</button>
-							
 						</li>)}
 				</ol>
 			</div>
